0758dffcd2494b7937dbd7c73bd132857b57e833
JamesAnunda <cjanunda@gmail.com>
2020-04-12 14:53:21 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { about } from './pages/about';
+import { contact } from './pages/contact';
+import { error404 } from './pages/error404';
+import { AppLayout } from './components/appLayout';
+import { NavHeader } from './components/navHeader';
+
+//Vendor
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavHeader />
+            <AppLayout />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={about} />
+                <Route path="/contact" component={contact} />
+                <Route component={error404} />
+              </Switch>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
