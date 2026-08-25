4cebc8c633e1944cb3b19d0195c178de6cd4145e
Chris Anunda <james@jamesanunda.dev>
2026-08-25 15:56:28 -0400

    Update index.ts
    turn reusePort to false so when running npm dev it allow the connection instead of error catcher throing something

--- server/index.ts
+++ server/index.ts
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    reusePort: false,
   }, () => {
     log(`serving on port ${port}`);
   });
